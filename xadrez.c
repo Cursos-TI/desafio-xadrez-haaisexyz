a375b1a6684564f84596b164f1845ff7a464f10e
haaisexyz <deathstyleanonymous@gmail.com>
2025-06-06 16:21:58 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -2,44 +2,86 @@
 
 // Trabalho movimento das peças de xadrez - Murillo :)
 
-int main()
-{
 
-    int torre = 0; // Movimento da Torre (5 casas para a direita usando o for)
-
-    for (int torre = 0; torre < 5; torre++)
-    {
-
-        printf("Direita\n");
+void torre(int direita) { //  Torre indo pra direita usando recursividade
+    if (direita <= 0) {
+        return;
     }
 
-    int bispo = 0; // Movimento do Bispo (5 casas diagonais usando o while-do)
+    printf("Direita\n");
+    torre(direita - 1); 
+}
 
-    do
-    {
-        printf("Esquerda, Cima\n");
-        bispo++;
 
-    } while (bispo < 5);
+void bispo(int sobe) { // Bispo subindo em diagonal
+    if (sobe == 0)
+        return;
 
-    int rainha = 0; // Movimento da Rainha (8 Casas para a esquerda usando o while)
+    printf("Esquerda, Cima\n");
+    bispo(sobe - 1);
+}
 
-    while (rainha < 8)
 
-    {
-        rainha++;
-        printf("Esquerda\n");
-    }
-
-    int cavalocompleto = 1; // Movimento do Cavalo (em L, dois cima e um esquerda)
-
-    while (cavalocompleto--)
-    {
-        for (int baixo = 0; baixo < 2; baixo++)
-        {
-            printf("Baixo\n");
+void bispo2(int bispo) {
+    for (int i = 0; i <= bispo; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Esquerda, Cima\n"); 
         }
-        printf("Esquerda\n");
     }
+}
+
+
+void rainha3(int esquerda) { // Rainha indo pra esquerda
+    if (esquerda < 1) return;
+    printf("Esquerda\n");
+    rainha3(esquerda - 1); 
+}
+
+
+void cavalo2() { // Cavalo se mexendo em L
+    for (int i = 0; i < 3; i++) {
+        if (i == 0) {
+            printf("Cima\n");
+        }
+        else if (i == 1) {
+            printf("Cima\n"); 
+        }
+        else {
+            printf("Direita\n"); 
+        }
+    }
+}
+
+int main() {
+
+    // Variaveis
+    int torrecasas = 5;
+    int bispoqtd = 5;
+    int rainhacontas = 8;
+
+    
+    printf("Movimento Da Torre:\n"); // Movimento Da Torre
+    torre(torrecasas);
+    printf("\n");
+
+    
+    bispo(bispoqtd); // Movimento do bispo
+    printf("\n");
+
+    
+    printf("Bispo Com For Aninhado:\n");
+    bispo2(bispoqtd);
+    printf("\n");
+
+    
+    printf("Rainha Se Movendo:\n"); // Movimento da Rainha
+    rainha3(rainhacontas);
+    printf("\n");
+
+    
+    printf("Movimento Do Cavalo:\n"); // Movimento do cavalo em L 
+    cavalo2();
+    printf("\n");
+
     return 0;
 }
